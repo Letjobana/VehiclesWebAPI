3511551f06c372208d9e5575a6a79f5dedbd1248
Letjobana <suzanm@gerbertech.co.za>
2021-10-08 04:58:50 +0200

    Added User Model

---
+++ FleetWebApi/FleetWebApi/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace FleetWebApi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string IdNumber { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
